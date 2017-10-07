9f881fcdb4a6a69e47904e9733a818f387f8259b
Christopher Jereza <cjereza@berkeley.edu>
2017-10-07 04:44:25 -0700

    added risk score and most versatile stocks

--- project/data/blackrockAPItest.py
+++ project/data/blackrockAPItest.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import numpy as np
+import Queue
 """
 For the examples we are using 'requests' which is a popular minimalistic python library for making HTTP requests.
 Please use 'pip install requests' to add it to your python libraries.
@@ -9,19 +10,24 @@ Please use 'pip install requests' to add it to your python libraries.
 def dump(js):
     print json.dumps(js, indent=2)
 
-portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'SQM~50|AAPL~50', 'calculateRisk': 'true'})
+portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'GOOG~20|AAPL~20|MU~20|NVDA~20|NTDOY~20', 'calculateRisk': 'true'})
 jsonObject = portfolioAnalysisRequest.json()
 holdings = jsonObject['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['holdings']
 tickers = []
 riskValues = []
 myList = []
+q = Queue.PriorityQueue()
 for holding in holdings:
     ticker = str(holding['ticker'])
     tickers.append(ticker)
+
     riskData = holding['riskData']
-    risk = (ticker, riskData['totalRisk'])
+    riskScore = riskData['totalRisk']
+    risk = (ticker, riskScore)
     riskValues.append(risk)
+    q.put(ticker, riskData['totalRisk'])
     myList.append(riskData['totalRisk'])
+
 
 mean_duration = np.mean(myList)
 std_dev_one_test = np.std(myList)
@@ -30,5 +36,6 @@ def drop_outliers(x):
         return x
 myList = filter(drop_outliers, myList)
 print('Your risk score: ', np.mean(myList))
-
-#print 'TICKERS: ', tickers
+print('Your three most volatile stocks: ')
+for _ in range(0, 3):
+    print(q.get())
