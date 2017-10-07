6e9a93fab57e13f5763070a4490b92bb42531d10
ryanlawler <ryanlawler@berkeley.edu>
2017-10-07 00:54:47 -0700

    here

--- project/data/blackrockAPI.py
+++ project/data/blackrockAPI.py
@@ -1,10 +1,16 @@
 import requests
-
 """
 For the examples we are using 'requests' which is a popular minimalistic python library for making HTTP requests.
 Please use 'pip install requests' to add it to your python libraries.
 """
 
-portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'BLK~25|AAPL~25|IXN~25|MALOX~25'})
-#print(portfolioAnalysisRequest.text) # get in text string format
-print portfolioAnalysisRequest.json() # get as json object
+portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/performance", params= {'identifiers':"AAPL"})
+# print(portfolioAnalysisRequest.text # get in text string format
+performance = portfolioAnalysisRequest.json()[u'resultMap'][u'RETURNS'][0][u'performanceChart']
+
+f = open('stock_data', 'w')
+f.write(str(performance))
+count = 0
+for _ in performance:
+    count += 1
+print count
