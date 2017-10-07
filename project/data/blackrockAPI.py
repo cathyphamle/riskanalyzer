05641d7a6c1e0015b936b84d26e16f7c190949cc
ryanlawler <ryanlawler@berkeley.edu>
2017-10-06 23:28:00 -0700

    stuff

--- project/data/blackrockAPI.py
+++ project/data/blackrockAPI.py
@@ -6,5 +6,5 @@ Please use 'pip install requests' to add it to your python libraries.
 """
 
 portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'BLK~25|AAPL~25|IXN~25|MALOX~25'})
-print(portfolioAnalysisRequest.text) # get in text string format
-print(portfolioAnalysisRequest.json) # get as json object
+#print(portfolioAnalysisRequest.text) # get in text string format
+print portfolioAnalysisRequest.json() # get as json object
