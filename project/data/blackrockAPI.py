8885e385e0b26468452cb818b00448be2e3d3b11
ryanlawler <ryanlawler@berkeley.edu>
2017-10-07 02:00:35 -0700

    stuff

--- project/data/blackrockAPI.py
+++ project/data/blackrockAPI.py
@@ -4,7 +4,6 @@ For the examples we are using 'requests' which is a popular minimalistic python 
 Please use 'pip install requests' to add it to your python libraries.
 """
 
-<<<<<<< HEAD
 portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/performance", params= {'identifiers':"AAPL"})
 # print(portfolioAnalysisRequest.text # get in text string format
 performance = portfolioAnalysisRequest.json()[u'resultMap'][u'RETURNS'][0][u'performanceChart']
@@ -15,8 +14,4 @@ count = 0
 for _ in performance:
     count += 1
 print count
-=======
-portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'BLK~25|AAPL~25|IXN~25|MALOX~25'})
-#print(portfolioAnalysisRequest.text) # get in text string format
-print (portfolioAnalysisRequest.json()) # get as json object
->>>>>>> 054ea0b01a13a874938831e52f2be6cbcf5de89f
+
